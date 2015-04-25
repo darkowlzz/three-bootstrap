2a2a1abf889880b3938d7ed03422a71c398a4135
sunny <indiasuny000@gmail.com>
2015-04-25 16:44:39 +0530

    Auto resize added.

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
 var scene, camera, renderer;
 var geometry, material, mesh;
@@ -6,35 +6,47 @@ var geometry, material, mesh;
 init();
 animate();
 
-function init() {
+function init () {
 
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.z = 1000;
 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+  geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-    document.body.appendChild( renderer.domElement );
+  document.body.appendChild( renderer.domElement );
+
+  // Auto resize the rendering area when resize event is detected.
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+
+function animate () {
+
+  requestAnimationFrame( animate );
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
+  renderer.render( scene, camera );
 
 }
 
-function animate() {
 
-    requestAnimationFrame( animate );
+// Resize the rendering area
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
-    renderer.render( scene, camera );
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 })();
